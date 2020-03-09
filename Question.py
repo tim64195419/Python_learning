31321ae6c9a3f56c5e38e446d822757a6faea12d
tim64195419 <tim64195419@gmail.com>
2020-03-09 15:18:18 +0800

    no message

--- Question.py
+++ Question.py
@@ -185,3 +185,31 @@ print('fib:12 = %d' % fib(12))
 
 # dp動態規劃例子：子列表元素之和的最大值
 
+'''
+说明：子列表指的是列表中索引（下标）连续的元素构成的列表；列表中的元素是int类型，可能包含正整数、0、负整数；程序输入列表中的元素，输出子列表元素求和的最大值，例如：
+
+输入：1 -2 3 5 -3 2
+
+输出：8
+
+输入：0 -2 3 5 -1 2
+
+输出：9
+
+输入：-9 -2 -3 -5 -3
+
+输出：-2
+'''
+
+def main():
+    items = list(map(int,input().split()))
+    size = len(items)
+    overall,partial = {},{}
+    overall[size -1] = partial[size -1] = items[size -1]
+    for i in range(size -2,-1,-1):
+        partial[i] = max(items[i], partial[i + 1] + items[i])
+        overall[i] = max(partial[i], overall[i + 1])
+    print(overall[0])
+
+if __name__ == '__main__':
+    main()
--- practice.py
+++ practice.py
@@ -8,3 +8,13 @@ words = [
 ]
 counter = Counter(words)
 print(counter.most_common(3))
+
+
+
+items1 = list(map(lambda x: x ** 2, filter(lambda x: x % 2, range(1, 20))))
+items2 = [x ** 2 for x in range(1, 20) if x % 2]
+
+
+
+
+print(items2)
