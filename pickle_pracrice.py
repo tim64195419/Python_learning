22a222f277eb07819e041ee864ef341bda55bbaa
tim64195419 <tim64195419@gmail.com>
2020-03-27 15:51:51 +0800

    no message

--- pickle_pracrice.py
+++ pickle_pracrice.py
@@ -1,13 +1,15 @@
+# pickle 是一個緩存的方式可以寫入data,一段時間後再呼叫出來直接使用。
 import pickle
 
 data = {'foo': [1, 2, 3],
         'bar': ('Hello', 'world!'),
         'baz': True}
+# 新建一個data.pkl 並且存入data
 jar = open('data.pkl', 'wb')
 pickle.dump(data, jar) # write the pickled data to the file jar
 jar.close()
 
-
+# 打開data.pkl file 並且載入file data
 pkl_file = open('data.pkl', 'rb') # connect to the pickled data
 data = pickle.load(pkl_file) # load it into a variable
 print(data)
